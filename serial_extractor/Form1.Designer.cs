8a8cf0686dacd7d7d81f84ed8cbf2eab9391d472
sulaiman al qusaimi <Suleiman.ahmed2001@gmail.com>
2022-09-03 10:43:14 +0400

    initial commit

---
+++ serial_extractor/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace serial_extractor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSerial = new System.Windows.Forms.Label();
+            this.txtSerial = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSerial
+            // 
+            this.lblSerial.AutoSize = true;
+            this.lblSerial.Location = new System.Drawing.Point(0, 0);
+            this.lblSerial.Name = "lblSerial";
+            this.lblSerial.Size = new System.Drawing.Size(112, 17);
+            this.lblSerial.TabIndex = 0;
+            this.lblSerial.Text = "الرقم التسلسلي:";
+            // 
+            // txtSerial
+            // 
+            this.txtSerial.Location = new System.Drawing.Point(8, 8);
+            this.txtSerial.Name = "txtSerial";
+            this.txtSerial.ReadOnly = true;
+            this.txtSerial.Size = new System.Drawing.Size(100, 24);
+            this.txtSerial.TabIndex = 1;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(0, 0);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "نسخ";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtSerial);
+            this.Controls.Add(this.lblSerial);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Serial Extractor";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSerial;
+        private System.Windows.Forms.TextBox txtSerial;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ serial_extractor/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace serial_extractor
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Process p = new Process();
+                ProcessStartInfo info = new ProcessStartInfo();
+
+                info.FileName = "wmic";
+                info.Arguments = "bios get serialnumber";
+                info.UseShellExecute = false;
+                info.RedirectStandardOutput = true;
+                info.CreateNoWindow = true;
+                p.StartInfo = info;
+                p.Start();
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                txtSerial.Text = output.Split('\n')[1];
+            }
+            catch (Exception exc)
+            {
+
+                MessageBox.Show("حدث خطأ غير معروف أثناء محاولة استخراج الرقم التسلسلي لجهازك", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtSerial.Text);
+            MessageBox.Show("تم النسخ بنجاح", "نجاح");
+            txtSerial.Focus();
+        }
+    }
+}
